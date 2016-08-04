8d85871ca4f070cd35eb04945e29bfb22b1e6722
Svieg <hugo.genesse@polymtl.ca>
2016-08-04 14:19:04 -0400

    v0.1.1

--- polyhack_bot.py
+++ polyhack_bot.py
@@ -1,5 +1,5 @@
 """
-Polyhack bot: IRC bot for #polyhack_ on freenode
+Polyhack bot: IRC bot
 """
 #!/usr/bin/env python3
 
@@ -37,7 +37,7 @@ PAYLOAD = {"longUrl": "",
            "apiKey": "YOUR PO.ST API KEY",  # You need to change that
            "format": "txt"}
 
-GIRLS = {"fille", "chick", "fanny", "melitza", "femme"}
+GIRLS = {"fille", "chick", "femme"}
 
 READ_BUFFER = ""
 
@@ -46,7 +46,7 @@ S.connect((HOST, PORT))
 
 S.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
 S.send(bytes("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME), "UTF-8"))
-S.send(bytes("JOIN #polyhack_\r\n", "UTF-8"))
+S.send(bytes("JOIN <YOUR_CHANNEL>\r\n", "UTF-8"))
 S.send(bytes("PRIVMSG %s :Hello Master\r\n" % MASTER, "UTF-8"))
 
 while 1:
